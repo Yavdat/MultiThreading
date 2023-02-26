b77da9b11bbcaf067b57411d7926e847b7ac979c
Yavdat <yavdat.it@gmail.com>
2023-02-26 23:21:27 +0300

    Threads and class methods

--- MultiThreading.md
+++ MultiThreading.md
@@ -1,16 +1,27 @@
 ## MultiThreading C++
 ### Объект thread
-thread th(Func); - вызываем функцию в отдельном потоке  
+>thread th(Func); - вызываем функцию в отдельном потоке  
 th.detach(); - отвязываем отдельный поток от главного потока  
 th.join(); - привязываем, и теперь главный поток будет дожидаться завершения дочернего потока  
 
 ### Передача данных через параметры функции потоку
-void FuncWithParams(int par1, int par2)  
+>void FuncWithParams(int par1, int par2)  
 thread th(FuncWithParams, par1, par2)
 
 ### Передача и возврат результата из потока с помощью ссылки
-void FuncWithRefParam(int &refPar)  
+>void FuncWithRefParam(int &refPar)  
 thread th(FuncWithRefParam, std::ref(num)) - без использования std::ref возникнет ошибка
 
+### Использование анонимной функции и возврат результата из потока
+>int FuncWithReturningValue(int num)  
+int result;   
+thread th4([&result](){result = FuncWithReturningValue(10);});
 
-
+### Потоки и методы класса
+>MyClass m;  
+string par1 = "params";  
+int par2 = 5;
+int result;    
+thread th5([&](){m.Work();});  
+thread th6(&MyClass::WorkWithPar1, m, par1);  
+thread th7([&](){result = m.WorkWithPar1AndRetVal(par2);});  
--- main.cpp
+++ main.cpp
@@ -42,6 +42,70 @@ void FuncWithRefParam(int &num)
     cout << "END FUNC WITH REF PARAM  " << this_thread::get_id() << endl;
 }
 
+int FuncWithReturningValue(int num)
+{
+    int result;
+    cout << "START FUNC WITH RETURNING VALUE " << this_thread::get_id() << endl;
+
+    num = num * 2;
+    cout << "ID потока = " << this_thread::get_id() << "\tFunc\t" << "numX2 = " << num << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+
+    cout << "START FUNC WITH RETURNING VALUE " << this_thread::get_id() << endl;
+    return num;
+}
+
+class MyClass
+{
+private:
+    /* data */
+public:
+    MyClass(/* args */);
+    ~MyClass();
+
+    void Work()
+    {
+        cout << "START FUNC WORK " << this_thread::get_id() << endl;
+
+        cout << "ID потока = " << this_thread::get_id() << "\tFunc\t" << "WORK " << endl;
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "END FUNC WORK " << this_thread::get_id() << endl;
+    }
+
+    void WorkWithPar1(string par1)
+    {
+        cout << "START FUNC WorkWithPar1 " << this_thread::get_id() << endl;
+
+        cout << "ID потока = " << this_thread::get_id() << "\tFunc\t" << "WORK with par1=" << par1 << endl;
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "END FUNC WorkWithPar1 " << this_thread::get_id() << endl;
+    }
+
+    int WorkWithPar1AndRetVal(int par1)
+    {
+        cout << "START FUNC WORK with returning VAL" << this_thread::get_id() << endl;
+        
+        cout << "ID потока = " << this_thread::get_id() << "\tFunc\t" << "WORK with par1=" << par1 << " and Return par1 * 2 = " << par1*2 << endl;
+        this_thread::sleep_for(chrono::milliseconds(2000));
+
+        par1 = par1 * 2;
+
+        cout << "END FUNC WORK with returning VAL " << this_thread::get_id() << endl;
+        return par1;
+    }
+};
+
+MyClass::MyClass(/* args */)
+{
+}
+
+MyClass::~MyClass()
+{
+}
+
+
 
 int main()
 {
@@ -51,10 +115,20 @@ int main()
 
     int num = 5;
 
-    thread th1(Func);
-    thread th2(FuncWithParams, 1000, 2000);
-    thread th3(FuncWithParams, 1000, 1500);
-    thread th4(FuncWithRefParam, std::ref(num));
+    int result;
+
+    // thread th1(Func);
+    // thread th2(FuncWithParams, 1000, 2000);
+    // thread th3(FuncWithRefParam, std::ref(num));
+    // thread th4([&result](){result = FuncWithReturningValue(10);});
+    
+    MyClass m;
+    string par1 = "params";
+    int par2 = 5; 
+    thread th5([&](){m.Work();});
+    thread th6(&MyClass::WorkWithPar1, m, par1);
+    thread th7([&](){result = m.WorkWithPar1AndRetVal(par2);});
+    
 
     for (size_t i=0; i<10; i++)
     {
@@ -63,9 +137,13 @@ int main()
     }
 
     cout << "END MAIN" << endl;
-    th1.join();
-    th2.join();
-    th3.join(); 
-    th4.join();
+    // th1.join();
+    // th2.join();
+    // th3.join(); 
+    // th4.join();
+    th5.join();
+    th6.join();
+    th7.join();
+    cout << "NUM = " << num << endl;
     return 0;
 }
