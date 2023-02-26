5267889ed1b0758022a67ac3c8adef44b105aeee
Yavdat <yavdat.it@gmail.com>
2023-02-26 04:12:50 +0300

    add FuncWithRefParam

--- main.cpp
+++ main.cpp
@@ -28,6 +28,20 @@ void FuncWithParams(int t1, int t2)
     cout << "END FUNC WITH PARAMS " << this_thread::get_id() << endl;
 }
 
+void FuncWithRefParam(int &num)
+{
+    cout << "START FUNC WITH REF PARAM " << this_thread::get_id() << endl;
+
+    for(size_t i = 0; i < 10; i++)
+    {
+        num = num + i;
+        cout << "ID потока = " << this_thread::get_id() << "\tFunc\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(2000));
+    }
+    this_thread::sleep_for(chrono::milliseconds(4000));
+    cout << "END FUNC WITH REF PARAM  " << this_thread::get_id() << endl;
+}
+
 
 int main()
 {
@@ -35,9 +49,12 @@ int main()
 
     cout << "START MAIN" << endl;
 
+    int num = 5;
+
     thread th1(Func);
     thread th2(FuncWithParams, 1000, 2000);
     thread th3(FuncWithParams, 1000, 1500);
+    thread th4(FuncWithRefParam, std::ref(num));
 
     for (size_t i=0; i<10; i++)
     {
@@ -49,5 +66,6 @@ int main()
     th1.join();
     th2.join();
     th3.join(); 
+    th4.join();
     return 0;
 }
